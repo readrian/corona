017a589d167435ad2485642148de04e7c14ca836
Adrian Rendl <arendl1405@gmail.com>
2021-03-28 16:37:46 +0200

    Cleaned up code

--- public/index.js
+++ public/index.js
@@ -5,24 +5,19 @@ function numberWithCommas(x) {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 }
 
-async function setup() {
-  //Infections
-  const itx = document.getElementById('infectionsDE').getContext('2d');
-  const covid = await getData();
-  document.getElementById('totalInfGlobal').innerHTML = `Weltweite Infektionen: ${numberWithCommas(covid.global.Global.TotalConfirmed)}`;
-  document.getElementById('totalDeathGlobal').innerHTML = `Weltweite Tode: ${numberWithCommas(covid.global.Global.TotalDeaths)}`;
-  document.getElementById('totalRecGlobal').innerHTML = `Weltweit genesen: ${numberWithCommas(covid.global.Global.TotalRecovered)}`;
-  const myChartI = new Chart(itx, {
+function makeChart(covid, id, label, path, color) {
+  const mtx = document.getElementById(id).getContext('2d');
+  const myChartNew = new Chart(mtx, {
     type: 'line',
     data: {
       labels: covid.date,
       datasets: [
         {
-          label: 'Covid Infections Germany',
-          data: covid.infData,
+          label: label,
+          data: path,
           fill: true,
-          borderColor: 'rgba(255, 99, 132, 1)',
-          backgroundColor: 'rgba(255, 99, 132, 1)',
+          borderColor: color,
+          backgroundColor: color,
           borderWidth: 1,
           pointRadius: 0,
 
@@ -61,57 +56,19 @@ async function setup() {
       }
     }
   });
+}
 
-  //Deaths
-  const dtx = document.getElementById('deathDE').getContext('2d');
-  const myChartD = new Chart(dtx, {
-    type: 'line',
-    data: {
-      labels: covid.date,
-      datasets: [
-        {
-          label: 'Covid Deaths Germany',
-          data: covid.death,
-          fill: true,
-          borderColor: 'rgba(12, 12, 12, 1)',
-          backgroundColor: 'rgba(12, 12, 12, 1)',
-          borderWidth: 1,
-          pointRadius: 0
-        }
-      ]
-    },
-    options: {
-      tooltips: {
-        callbacks: {
-          label: function (tooltipItem, data) {
-            var value = data.datasets[0].data[tooltipItem.index];
-            value = value.toString();
-            value = value.split(/(?=(?:...)*$)/);
-            value = value.join('.');
-            return value;
-          }
-        }
-      },
-      scales: {
-        yAxes: [{
-          ticks: {
-            beginAtZero: true,
-            userCallback: function (value, index, values) {
-              // Convert the number to a string and splite the string every 3 charaters from the end
-              value = value.toString();
-              value = value.split(/(?=(?:...)*$)/);
-              value = value.join('.');
-              return value;
-            }
-          }
-        }],
-        xAxes: [{
-          ticks: {
-          }
-        }]
-      }
-    }
-  });
+
+
+async function setup() {
+  //Infections
+  const itx = document.getElementById('infectionsDE').getContext('2d');
+  const covid = await getData();
+  document.getElementById('totalInfGlobal').innerHTML = `Weltweite Infektionen: ${numberWithCommas(covid.global.Global.TotalConfirmed)}`;
+  document.getElementById('totalDeathGlobal').innerHTML = `Weltweite Tode: ${numberWithCommas(covid.global.Global.TotalDeaths)}`;
+  document.getElementById('totalRecGlobal').innerHTML = `Weltweit genesen: ${numberWithCommas(covid.global.Global.TotalRecovered)}`;
+  makeChart(covid, 'infectionsDE', 'Covid Infections Germany', covid.infData, 'rgba(255, 99, 132, 1)')
+  makeChart(covid, 'deathDE', 'Covid Deaths Germany', covid.death, 'rgba(12, 12, 12, 1)')
 
 
   async function getData() {
